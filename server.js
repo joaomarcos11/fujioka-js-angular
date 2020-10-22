4b811602ac629251fbe2607004135a0f286f6dae
Joao Marcos <joaomarcos11.tech@gmail.com>
2020-10-21 21:44:29 -0300

    CRUD de Product, adicao de paginacao e cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+// Conexão com MongoDB
 mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', { 
   useUnifiedTopology: true,
   useNewUrlParser: true
@@ -11,12 +15,10 @@ mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', {
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
+app.use('/api', require('./src/routes'));
 
-app.get('/', (req, res) => {
-  Product.create({title: 'Guitarra', description: 'Fender Stratocaster'});
+const port = 3333;
 
-  res.send('Hello World');
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
 });
-
-app.listen(3333);
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
 
 const ProductSchema = new mongoose.Schema({
   title: {
@@ -14,5 +15,7 @@ const ProductSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+ProductSchema.plugin(mongoosePaginate);
 
 mongoose.model('Product', ProductSchema);
